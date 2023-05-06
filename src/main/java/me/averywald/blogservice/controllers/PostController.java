6a18b4a7257e05bdafad8658d6d05f8ad8293353
averywald <averywald.dev@gmail.com>
2023-05-06 12:54:56 -0500

    cleanup; added controller top-level redirect ligature

--- src/main/java/me/averywald/blogservice/controllers/PostController.java
+++ src/main/java/me/averywald/blogservice/controllers/PostController.java
@@ -1,17 +1,22 @@
 package me.averywald.blogservice.controllers;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import me.averywald.blogservice.DTOs.PostDTO;
 import me.averywald.blogservice.repositories.PostRepository;
 
 @Controller
+@RequestMapping("/blog")
 public class PostController {
 
     // TODO: implement pagination, in a service?
@@ -19,9 +24,23 @@ public class PostController {
     @Autowired private PostRepository postRepo;
 
     /**
+     * Catch the controller's top-level request
+     * "/blog", and redirects it to "/blog/posts".
+     */
+    @GetMapping
+    public void redirectToPostsEndpoint(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        try {
+            resp.sendRedirect(req.getServletPath() + "/posts");
+        }
+        catch (IOException e) {
+            throw e;
+        }
+    }
+
+    /**
      * @return a list of Post DTOs
      */
-    @GetMapping("/blog")
+    @GetMapping("/posts")
     @ResponseBody
     public List<PostDTO> getPosts() {
         return this.postRepo.getPostList();
@@ -31,7 +50,7 @@ public class PostController {
      * @param author the name of the author to find posts for
      * @return list of post DTOs
      */
-    @GetMapping("/blog/author/{author}")
+    @GetMapping("/posts/author/{author}")
     @ResponseBody
     public List<PostDTO> getPostsByAuthor(@PathVariable String author) {
         return this.postRepo.getPostsByAuthor(author);
